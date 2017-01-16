3709757eb09132de31af717522f2d093376dd666
bntj <asuschenya@gmail.com>
2017-01-17 01:07:11 +0300

    little of change view

--- src/by/asushenya/sela/View.java
+++ src/by/asushenya/sela/View.java
@@ -9,13 +9,13 @@ public class View {
 	
 		Controller controller = new Controller();
 		
-		//System.out.println(controller.executeTask("REGISTERED_NEW_USER Artyom 1111")); 
-		//System.out.println(controller.executeTask("SIGN_IN Artyom 1111")); 
-		//System.out.println(controller.executeTask("ADD_NEW_EQUIPMENT racket tennis 30 10")); 
-		//System.out.println(controller.executeTask("GET_ALL_EQUIPMENT ")); 
-		//System.out.println(controller.executeTask("RENT_EQUIPMENT 5 8 30 2 ")); 
+		System.out.println(controller.executeTask("REGISTERED_NEW_USER Artyom 1111")); 
+		System.out.println(controller.executeTask("SIGN_IN Artyom 1111")); 
+		System.out.println(controller.executeTask("ADD_NEW_EQUIPMENT racket tennis 30 10")); 
+		System.out.println(controller.executeTask("GET_ALL_EQUIPMENT ")); 
+		System.out.println(controller.executeTask("RENT_EQUIPMENT 5 8 30 2 ")); 
 		System.out.println(controller.executeTask("GET_OWER_REPORT ")); 
-		//System.out.println(controller.executeTask("THIS_COMMAND_IS_NOT_EXIST ")); 
+		System.out.println(controller.executeTask("THIS_COMMAND_IS_NOT_EXIST ")); 
 		
 	}
 
